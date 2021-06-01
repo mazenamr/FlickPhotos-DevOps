2c3bf7f2bf536ee797663bf20beffbe8cc7c5d2c
mazenamr <14795777+mazenamr@users.noreply.github.com>
2021-06-02 00:55:50 +0200

    Add creation of log folders

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,6 +3,13 @@
 cd $HOME
 
 [ ! -d "logs" ] && mkdir logs
+cd logs
+[ ! -d "time" ] && mkdir time
+[ ! -d "api" ] && mkdir api
+[ ! -d "apidocs" ] && mkdir apidocs
+[ ! -d "web" ] && mkdir web
+
+cd $HOME
 
 [ ! -d "flags" ] && mkdir flags
 rm -rf flags/*
@@ -15,6 +22,7 @@ git checkout main
 git pull origin
 
 cd $HOME
+
 [ ! -d "secrets" ] && git clone git@git.flick.photos:secrets.git
 cd secrets
 git clean -fxd

eeeb834bb7752acf5fdddd6e20500072639c094d
mazenamr <14795777+mazenamr@users.noreply.github.com>
2021-06-01 15:07:53 +0200

    Update scripts

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,6 +5,14 @@ cd $HOME
 [ ! -d "flags" ] && mkdir flags
 rm -rf flags/*
 
+[ ! -d "files" ] && git@github.com:mazenamr/FlickPhotos-DevOps.git files
+cd files
+git clean -fxd
+git reset --hard
+git checkout main
+git pull origin
+
+cd $HOME
 [ ! -d "secrets" ] && git clone git@git.flick.photos:secrets.git
 cd secrets
 git clean -fxd

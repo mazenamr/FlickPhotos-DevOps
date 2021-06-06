4d84b7103bec06e29fed8f26f3ecdaf0625da7b7
mazenamr <14795777+mazenamr@users.noreply.github.com>
2021-06-07 01:50:12 +0200

    Update Deploy Script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,11 @@
 cd $HOME/files/scripts
 
 chmod +x *.sh
-./update.sh && ./api.sh; ./apidocs.sh; ./web.sh;
+
+./update.sh || exit -1
+
+./api-build.sh && ./api-test.sh && ./api-deploy.sh
+
+./apidocs-build.sh && ./apidocs-test.sh && ./apidocs-deploy.sh
+
+./web-build.sh && ./web-test.sh && ./web-deploy.sh

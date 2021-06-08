0f5f405d1e1758b73fd72e686d57afea4cd3fda5
mazenamr <14795777+mazenamr@users.noreply.github.com>
2021-06-09 01:25:33 +0200

    Add Monitoring

--- scripts/server.sh
+++ scripts/server.sh
@@ -24,6 +24,53 @@ cat .ssh/id_ed25519.pub >> .ssh/authorized_keys
 eval `ssh-agent -s`
 ssh-add .ssh/id_ed25519
 
+# # setup prometheus
+# sudo useradd --no-create-home --shell /bin/false prometheus
+# sudo mkdir /etc/prometheus
+# sudo mkdir /var/lib/prometheus
+# sudo chown prometheus:prometheus /etc/prometheus
+# sudo chown prometheus:prometheus /var/lib/prometheus
+# curl -LO https://github.com/prometheus/prometheus/releases/download/v2.27.1/prometheus-2.27.1.linux-amd64.tar.gz
+# tar xvfz prometheus-*.tar.gz
+# sudo cp prometheus-2.27.1.linux-amd64/prometheus /usr/local/bin/
+# sudo cp prometheus-2.27.1.linux-amd64/promtool /usr/local/bin/
+# sudo chown prometheus:prometheus /usr/local/bin/prometheus
+# sudo chown prometheus:prometheus /usr/local/bin/promtool
+# sudo cp -r prometheus-2.27.1.linux-amd64/consoles /etc/prometheus
+# sudo cp -r prometheus-2.27.1.linux-amd64/console_libraries /etc/prometheus
+# sudo chown -R prometheus:prometheus /etc/prometheus/consoles
+# sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
+# rm -rf prometheus-2.27.1.linux-amd64.tar.gz prometheus-2.27.1.linux-amd64
+# sudo cp ./files/prometheus/prometheus.service /etc/systemd/system/prometheus.service
+# sudo systemctl daemon-reload
+# sudo systemctl start prometheus
+# sudo systemctl enable prometheus
+
+# #set grafana
+# sudo apt-get install -y apt-transport-https
+# sudo apt-get install -y software-properties-common wget
+# wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+# echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+# sudo apt-get update
+# sudo apt-get install grafana
+# sudo systemctl daemon-reload
+# sudo systemctl start grafana-server
+# sudo systemctl enable grafana-server
+
+# setup and run node exporter
+sudo useradd --no-create-home --shell /bin/false node_exporter
+curl -LO https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-amd64.tar.gz
+tar xvf node_exporter-1.1.2.linux-amd64.tar.gz
+sudo cp node_exporter-1.1.2.linux-amd64/node_exporter /usr/local/bin
+sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
+rm -rf node_exporter-1.1.2.linux-amd64.tar.gz node_exporter-1.1.2.linux-amd64
+sudo cp ./files/prometheus/node_exporter.service /etc/systemd/system/node_exporter.service
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
+sudo systemctl enable node_exporter
+
+cd $HOME
+
 # install node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
@@ -58,12 +105,17 @@ cp secrets/settings/msmtprc /etc/
 git clone git@github.com:mazenamr/FlickPhotos-DevOps.git files
 
 # setup nginx
+cp secrets/nginx/.htpasswd /etc/nginx/.htpasswd
 cp files/nginx/api /etc/nginx/sites-available/
 cp files/nginx/web /etc/nginx/sites-available/
 cp files/nginx/files /etc/nginx/sites-available/
+# cp files/nginx/prometheus /etc/nginx/sites-available/
+# cp files/nginx/grafana /etc/nginx/sites-available/
 ln -sf /etc/nginx/sites-available/web /etc/nginx/sites-enabled/web
 ln -sf /etc/nginx/sites-available/api /etc/nginx/sites-enabled/api
 ln -sf /etc/nginx/sites-available/files /etc/nginx/sites-enabled/files
+# ln -sf /etc/nginx/sites-available/prometheus /etc/nginx/sites-enabled/prometheus
+# ln -sf /etc/nginx/sites-available/grafana /etc/nginx/sites-enabled/grafana
 cp -f files/nginx/nginx.conf /etc/nginx/nginx.conf
 service nginx restart
 
@@ -76,9 +128,7 @@ iptables -F
 # setup ssl
 # certbot --nginx --agree-tos --redirect -n -m "admin@flick.photos" --keep --expand -d "flick.photos" -d "www.flick.photos"
 # certbot --nginx --agree-tos --no-redirect -n -m "admin@flick.photos" --keep --expand -d "api.flick.photos"
-# certbot --nginx --agree-tos --redirect -n -m "admin@flick.photos" --keep --expand -d "files.flick.photos" -d "mail.flick.photos"
-# certbot --nginx --agree-tos --redirect -n -m "admin@flick.photos" --keep --expand -d "flick.photos" -d "www.flick.photos"
-# certbot --nginx --agree-tos --no-redirect -n -m "admin@flick.photos" --keep --expand -d "api.flick.photos"
+# certbot --nginx --agree-tos --no-redirect -n -m "admin@flick.photos" --keep --expand -d "flick.photos" -d "www.flick.photos" -d "api.flick.photos" -d "files.flick.photos" -d "prometheus.flick.photos" -d "grafana.flick.photos"
 certbot --nginx --agree-tos --no-redirect -n -m "admin@flick.photos" --keep --expand -d "flick.photos" -d "www.flick.photos" -d "api.flick.photos" -d "files.flick.photos"
 
 # setup mongodb
